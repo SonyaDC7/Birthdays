f78d331ba2fcd56171bd72f8bc0b3360960aaa9b
Sonya <sonyacisse7@gmail.com>
2025-08-08 09:51:41 -0400

    created a friend swift file

---
+++ Friend.swift
@@ -0,0 +1,19 @@
+//
+//  Friend.swift
+//  Birthdays
+//
+//  Created by Scholar on 8/8/25.
+//
+
+import Foundation
+
+class Friend { //declare class
+    var name: String //property
+    var birthday: Date //property
+    
+    
+    init(name: String, birthday: Date) { //start initializer
+        self.name = name
+        self.birthday = birthday
+    } //end initializer
+} //end friend class
